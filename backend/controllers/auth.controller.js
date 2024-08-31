3744a4b88dd735b79136a4e69cd9f653b5adb7b6
Shaswat Shah <shaswat.shah008@gmail.com>
2024-09-01 08:45:13 +1000

    Initial commit - Initialized project folder and created a user model

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+//signup controller
+export const signup = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log();
+    res.status(500).json();
+  }
+};
+
+//login controller
+export const login = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log();
+    res.status(500).json();
+  }
+};
+
+//logout controller
+export const logout = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log();
+    res.status(500).json();
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRouter from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
+
