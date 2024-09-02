747ce15ba67c72994b09f4090daf5322ef19f1ec
Shaswat Shah <shaswat.shah008@gmail.com>
2024-09-03 05:43:27 +1000

    Backend - Implemented product controllers for featured, recommended, and category-based product retrieval

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -20,7 +20,7 @@ export const getFeaturedProducts = async (req, res) => {
     let featuredProducts = await redis.get("featured_products");
 
     if (featuredProducts) {
-      return res.json(JSON.parse(featuredProducts));
+      return res.json(JSON.parse(featuredProducts)); // parse the stringified featuredProducts
     }
 
     // if not in redis
@@ -39,6 +39,56 @@ export const getFeaturedProducts = async (req, res) => {
   } catch {
     console.error(
       "Error in the getFeaturedProducts controller: ",
+      error.message
+    );
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+export const getRecommendedProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: { size: 3 },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          image: 1,
+          price: 1,
+        },
+      },
+    ]);
+
+    res.json(products);
+  } catch (error) {
+    console.error(
+      "Error in the getRecommendedProducts controller: ",
+      error.message
+    );
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+export const getProductsByCategory = async (req, res) => {
+  try {
+    const { category } = req.params;
+    const products = await Product.find({ category });
+
+    if (!products) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(
+      "Error in the getProductsByCategory controller: ",
       error.message
     );
     res
@@ -70,7 +120,7 @@ export const createProduct = async (req, res) => {
     });
 
     res.status(201).json(product);
-  } catch {
+  } catch (error) {
     console.error("Error in the createProduct controller: ", error.message);
     res
       .status(500)
@@ -103,6 +153,45 @@ export const deleteProduct = async (req, res) => {
     res.json({ message: "Product deleted successfully" });
   } catch {
     console.error("Error in the deleteProduct controller: ", error.message);
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+export const toggleFeaturedProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+
+    // Check if the product exists
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // Toggle the isFeatured field
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      { isFeatured: !product.isFeatured },
+      { new: true }
+    );
+
+    // update redis cache
+
+    try {
+      const featuredProducts = await Product.find({ isFeatured: true }).lean();
+      await redis.set("featured_products", JSON.stringify(featuredProducts));
+    } catch (error) {
+      console.error("Error updating Redis cache: ", error.message);
+    }
+
+    res.json({ updatedProduct });
+  } catch (error) {
+    console.error(
+      "Error in the toggleFeaturedProduct controller: ",
+      error.message
+    );
     res
       .status(500)
       .json({ message: "Internal server error", error: error.message });
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -2,15 +2,22 @@ import express from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 import {
   createProduct,
+  deleteProduct,
   getAllProducts,
   getFeaturedProducts,
+  getProductsByCategory,
+  getRecommendedProducts,
+  toggleFeaturedProduct,
 } from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
-router.post("/create", protectRoute, adminRoute, createProduct); 
-router.delete("/delete", protectRoute, adminRoute, deleteProduct); 
+router.get("/recommendations", getRecommendedProducts);
+router.get("/category/:category", getProductsByCategory);
+router.post("/create", protectRoute, adminRoute, createProduct);
+router.delete("/delete/:id", protectRoute, adminRoute, deleteProduct);
+router.patch("/toggle/:id", protectRoute, adminRoute, toggleFeaturedProduct)
 
 export default router;
